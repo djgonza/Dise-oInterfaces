7e0e7d405b2adcb76a5e3de38521514a190519b8
djgonza <davidjimenezgonzalez@MacBook-Pro-de-David.local>
2016-09-28 20:49:41 +0200

    Update

--- Ut1/js/reproductor.js
+++ Ut1/js/reproductor.js
@@ -1,7 +1,10 @@
 
-var reproductor = document.getElementById("reproductor");
-var btnPlay = document.getElementById("btnPlay");
-var btnMute = document.getElementById("btnMute");
+var reproductor = document.getElementById("reproductorV");
+var btnPlay = document.getElementById("btnPlayV");
+var btnMute = document.getElementById("btnMuteV");
+var timeStamp = document.getElementById("timeStampV");
+var timeTotal = document.getElementById("timeTotalV");
+var time;
 
 btnPlay.onclick = playPause;
 
@@ -25,6 +28,15 @@ function playPause () {
 		btnPlay.innerHTML = "pause";
 		btnPlay.style.color = "rgb(219, 17, 17)";
 		reproductor.play();
+		/* inicializamos los tiempos */
+		timeTotal.innerHTML = parseInt(reproductor.duration/60) + " : " + (reproductor.duration/60/60).toFixed(2)*100;
+		timeStamp.innerHTML = 0;
+		/* inicializamos el contador */
+		setInterval(function(){ 
+			var minutos = parseInt(reproductor.currentTime/60)
+			var segundos =  parseInt(reproductor.currentTime) - minutos * 60;
+			timeStamp.innerHTML = minutos + " : " + segundos;
+		}, 100);
 
 	}else{
 
@@ -32,11 +44,18 @@ function playPause () {
 			reproductor.play();
 			btnPlay.style.color = "rgb(219, 17, 17)";
 			btnPlay.innerHTML = "pause";
+			timeStamp.style.color = "rgb(60, 110, 234)";
+
 		}else{
 			reproductor.pause();
 			btnPlay.style.color = "rgb(60, 110, 234)";
 			btnPlay.innerHTML = "play_arrow";
+			timeStamp.style.color = "rgb(219, 17, 17)";
 		}
 	}	
 
+}
+
+function playTime () {
+	
 }
