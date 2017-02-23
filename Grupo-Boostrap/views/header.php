8235871902bb34fbeb8f44ef2f69ea847885a625
djgonza <davidjimenezgonza@gmail.com>
2017-02-23 11:08:12 +0100

    Update

--- Grupo-Boostrap/views/header.php
+++ Grupo-Boostrap/views/header.php
@@ -1,29 +1,29 @@
 <header>
-	<h1><a href="">Titulo</a></h1>
 	<nav class="row">
 		<h2>Titulo Del menu</h2>
-		<ul class="nav">
-			<li class="nav-item h-verde">
-				<a class="h-blanco" href="/primeros">
+		<img class="col-4 offset-md-1 img-fluid" src="http://placehold.it/150x75" alt="">
+		<ul class="nav col row offset-md-1 pull-md-1">
+			<li class="nav-item col h-bg-verde">
+				<a class="c-verde h-c-blanco h-clear-text-decoration" href="/primeros">
 					Primeros
 				</a>
 			</li>
-			<li class="nav-item h-morado">
-				<a class="c-blanco" href="/segundos">
+			<li class="nav-item col h-bg-naranja">
+				<a class="c-naranja h-c-blanco h-clear-text-decoration" href="/segundos">
 					Segundos
 				</a>
 			</li>
-			<li class="nav-item h-rosa">
-				<a class="c-blanco" href="/postres">
+			<li class="nav-item col h-bg-rosa">
+				<a class="c-rosa h-c-blanco h-clear-text-decoration" href="/postres">
 					Postres
 				</a>
 			</li>
-			<li class="nav-item h-naranja">
-				<a class="c-blanco" href="/contacto">
+			<li class="nav-item col h-bg-gris">
+				<a class="c-gris h-c-blanco h-clear-text-decoration" href="/contacto">
 					Contacto
 				</a>
 			</li>
-		</ul>	
+		</ul>
 	</nav>
 	<ul>
 		<li>Inicio/</li>
